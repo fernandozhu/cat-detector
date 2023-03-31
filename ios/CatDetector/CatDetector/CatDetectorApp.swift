915fded817fb74698ea55143696eaba3563c79c3
Fernando Zhu <fernandoisadev@gmail.com>
2023-03-31 17:20:20 +1300

    Add notification hub package; add configuration file

--- ios/CatDetector/CatDetector/CatDetectorApp.swift
+++ ios/CatDetector/CatDetector/CatDetectorApp.swift
@@ -6,6 +6,10 @@
 //
 
 import SwiftUI
+import WindowsAzureMessaging
+
+let connectionString = "Endpoint=sb://ntfns-cat-detector.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=Hi0QCG7g5X57NIAGtKDhoRiE0P1+sQSnih24zebCfIM="
+let hubName = "ntf-cat-detector"
 
 @main
 struct CatDetectorApp: App {
@@ -15,6 +19,18 @@ struct CatDetectorApp: App {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .onAppear{
+                    // MSNotificationHub.start(connectionString: connectionString, hubName: hubName)
+                    
+                    if let path = Bundle.main.path(forResource: "Notification", ofType: "plist") {
+                        if let propValues = NSDictionary(contentsOfFile: path) {
+                            let connectionString = propValues["ConnectionString"] as? String
+                            let hubName = propValues["HubName"] as? String
+                            
+                            print(hubName)
+                        }
+                    }
+                }
         }
     }
 }
