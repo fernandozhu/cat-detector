c1bd2c8755a99b0b8ed23d0d824b8d17d809cb65
Fernando Zhu <fernandoisadev@gmail.com>
2023-04-16 04:36:41 +1200

    Extend Azure Function to send push notification

--- infra/func-app/CatDetector/CatDetectionHandler/index.ts
+++ infra/func-app/CatDetector/CatDetectionHandler/index.ts
@@ -1,5 +1,10 @@
 import { AzureFunction, Context } from "@azure/functions";
 import { EventGridEvent } from "@azure/eventgrid";
+import {
+  NotificationHubsClient,
+  createAppleNotification,
+} from "@azure/notification-hubs";
+// import {  } from '@azure/notification-hubs'
 import { StorageBlobEventData } from "./models/event";
 import { CatDetectionRecord } from "./models/record";
 
@@ -11,7 +16,35 @@ const eventGridTrigger: AzureFunction = async function (
   context.log(eventGridEvent);
 
   const record = new CatDetectionRecord(eventGridEvent);
-  context.bindings.outputDocument = JSON.stringify(record.toJSON());
+
+  try {
+    context.bindings.outputDocument = JSON.stringify(record.toJSON());
+    //   context.bindings.outputNotification = JSON.stringify({
+    //     aps: { alert: "Notification Hub test notification" },
+    //   });
+
+    const connectionString = "";
+
+    const hubName = "ntf-cat-detector";
+
+    const client = new NotificationHubsClient(connectionString, hubName);
+
+    const messageBody = JSON.stringify({
+      aps: { alert: "Meow, a 🐱 is detected", sound: "bellring.aiff" },
+    });
+    const notification = createAppleNotification({
+      body: messageBody,
+      headers: {
+        "apns-priority": "10",
+        "apns-push-type": "alert",
+      },
+    });
+
+    context.log("*** SEND NOTIFICATION");
+    client.sendNotification(notification);
+  } catch (err) {
+    context.log(err);
+  }
 };
 
 export default eventGridTrigger;
