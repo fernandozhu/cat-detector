2f4e9d70b365b49d0cba57ab03d4c035ab87f603
Fernando Zhu <fernandoisadev@gmail.com>
2023-04-16 23:50:35 +1200

    Update env.ts

--- infra/func-app/CatDetector/CatDetectionHandler/env.ts
+++ infra/func-app/CatDetector/CatDetectionHandler/env.ts
@@ -1,4 +1,2 @@
-export const NOTIFICATION_CONNECTION =
-  "Endpoint=sb://ntfns-cat-detector.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=+ug0uQzh0W0mwQYdLNvoIQda62LOlHDuhmkIsgz1CDk=";
-
+export const NOTIFICATION_CONNECTION = "";
 export const HUB_NAME = "ntf-cat-detector";
