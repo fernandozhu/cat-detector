a2a72ca86abffa5ce7a1c36356637119fc4ad5eb
Fernando Zhu <fernandoisadev@gmail.com>
2023-04-19 21:11:50 +1200

    Remove unecessary func-app folder

--- infra/func-app/CatDetector/CatDetectionHandler/env.sample.ts
+++
@@ -1,3 +0,0 @@
-export const NOTIFICATION_CONNECTION =
-  "Endpoint=sb://ntfns-cat-detector.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=+ug0uQzh0W0mwQYdLNvoIQda62LOlHDuhmkIsgz1CDk=";
-export const HUB_NAME = "ntf-cat-detector";
