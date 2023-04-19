9d245e1fec9a337ea3046b25e7753a4444165bd8
Fernando Zhu <fernandoisadev@gmail.com>
2023-04-19 23:34:36 +1200

    Change script name

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -7,4 +7,4 @@ az account set --subscription $subscription
 az group create --name $resourceGroupName --location $location
 az deployment group create --resource-group $resourceGroupName --template-file $templateFile  --parameters funcAppName=$funcAppName
 
-source ./func-publish.sh
+source ./publish-func.sh
