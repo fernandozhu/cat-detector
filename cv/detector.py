a975c290b2721d15de4198c2e7023f7d2d5cfb5c
Fernando Zhu <fernandoisadev@gmail.com>
2023-04-24 04:17:00 +1200

    Upload local detection image to blob storage

--- cv/detector.py
+++ cv/detector.py
@@ -3,15 +3,16 @@ from datetime import datetime, timedelta
 import cv2
 from utils import upload_image_to_azure, create_detections_folder
 
-# TARGET_CLASS = "cat"
-TARGET_CLASS = "person"
+
+TARGET_CLASS = "cat"
+# TARGET_CLASS = "person"
+# TARGET_CLASS = "cup"
 COOLDOWN_DURATION = 10  # Minutes
 cooldown_end = None  # Cooldown end datetime
 
-'''
-# https://docs.ultralytics.com/usage/callbacks/#callbacks
-'''
+
 def on_predict_batch_end(predictor):
+    # https://docs.ultralytics.com/usage/callbacks/#callbacks
     global cooldown_end
 
     if cooldown_end is not None and datetime.now() < cooldown_end:
@@ -28,12 +29,12 @@ def on_predict_batch_end(predictor):
                     timedelta(minutes=COOLDOWN_DURATION)
                 # Save image locally
                 plot_img = result.plot()
-                filename = './detections/{}.png'.format(
+                file_path = './detections/{}.png'.format(
                     cooldown_start.strftime("%Y-%m-%d-%H-%M-%S"))
-                success = cv2.imwrite(filename, plot_img)
+                success = cv2.imwrite(file_path, plot_img)
                 if success:
                     # Save image to storage account
-                    upload_image_to_azure(filename)
+                    upload_image_to_azure(file_path)
 
 
 create_detections_folder()
@@ -43,4 +44,4 @@ model.add_callback("on_predict_batch_end", on_predict_batch_end)
 classes = [index for index in model.names if model.names[index] == TARGET_CLASS]
 # https://docs.ultralytics.com/modes/predict/#arguments
 results = model.predict(source="1", show=True, verbose=False,
-                        conf=0.9, classes=classes, save=True)
+                        conf=0.8, classes=classes, save=True)
